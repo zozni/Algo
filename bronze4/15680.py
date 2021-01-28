4c5e31e4717abcde9fb54fa779d9e1d7db12ce97
zozni <thekey1027@naver.com>
2021-01-29 01:25:32 +0900

    bronze4

---
+++ bronze4/15680.py
@@ -0,0 +1,8 @@
+# 연세대학교의 영문명은 YONSEI, 슬로건은 Leading the Way to the Future이다.
+# 이를 출력하는 프로그램을 작성해보도록 하자.
+
+a = int(input())
+if a==0:
+    print("YONSEI")
+elif a==1:
+    print("Leading the Way to the Future")
