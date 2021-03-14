5215f7bc06073286478e1e3c30d6b70038ef4187
zozni <thekey1027@naver.com>
2021-03-15 01:20:08 +0900

    bronze5

---
+++ bronze5/9654.py
@@ -0,0 +1,7 @@
+# 나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+print("""SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4         """)
