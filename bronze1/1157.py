33ab7a4f0fcd8fbb3268b307a21ddaacbddf813f
zozni <thekey1027@naver.com>
2021-03-19 15:26:31 +0900

    bronze1

--- bronze1/1157.py
+++ bronze1/1157.py
@@ -1,2 +1,15 @@
 # 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
 # 단, 대문자와 소문자를 구분하지 않는다.
+words = input().upper()
+unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
+
+cnt_list = []
+for x in unique_words:
+    cnt = words.count(x)
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append
+
+if cnt_list.count(max(cnt_list)) > 1:  # count 숫자 최대값이 중복되면
+    print('?')
+else:
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
